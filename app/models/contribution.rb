b30fcb5b151482ffaafbb7eb6e213a2296b805ac
carolinejobe <caroline@wlabel.co>
2018-07-22 17:25:44 -0400

    only show this year's guests and contributions

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,3 +1,5 @@
 class Contribution < ApplicationRecord
   belongs_to :guest, optional: true
+
+  scope :this_year, -> { where("created_at >= ?", 6.months.ago)}
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,7 @@ class Guest < ApplicationRecord
   has_one :contribution
   validates :name, presence: true
   accepts_nested_attributes_for :contribution
+
+  scope :this_year, -> { where("created_at >= ?", 6.months.ago)}
+
 end
